1b3552a782766dcf9b3fd999f359ab468ea51ba1
Edwin Jakobs <edwin@rndr.studio>
2020-04-29 19:58:41 +0200

    Add check for "pick me"

--- src/index.js
+++ src/index.js
@@ -6,39 +6,37 @@ const run = async () => {
     const gitHubToken = core.getInput('repo-token', { required: true });
     const octokit = new github.GitHub(gitHubToken);
 
-
-    console.log(github.context.payload);
-
     // Get repo and issue info
     const { repository, issue } = github.context.payload;
 
+    const commentUser = github.context.payload.comment.user.login;
+    const commentBody = github.context.payload.comment.body;
 
-    if (!issue) {
-        throw new Error(`Couldn't find issue info in current context`);
-    }
-    const [owner, repo] = repository.full_name.split('/');
+    if (commentBody.include("pick me")) {
+        if (!issue) {
+            throw new Error(`Couldn't find issue info in current context`);
+        }
+        const [owner, repo] = repository.full_name.split('/');
+        // Get issue assignees
+        const assigneesString = commentUser
+        const assignees = assigneesString
+            .split(',')
+            .map((assigneeName) => assigneeName.trim());
 
-    console.log(issue);
-
-    // Get issue assignees
-    const assigneesString = "edwinRNDR" //core.getInput('assignees', { required: true });
-    const assignees = assigneesString
-        .split(',')
-        .map((assigneeName) => assigneeName.trim());
-
-    // Assign issue
-    console.log(
-        `Assigning issue ${issue.number} to users ${JSON.stringify(assignees)}`
-    );
-    try {
-        await octokit.issues.addAssignees({
-            owner,
-            repo,
-            issue_number: issue.number,
-            assignees
-        });
-    } catch (error) {
-        core.setFailed(error.message);
+        // Assign issue
+        console.log(
+            `Assigning issue ${issue.number} to users ${JSON.stringify(assignees)}`
+        );
+        try {
+            await octokit.issues.addAssignees({
+                owner,
+                repo,
+                issue_number: issue.number,
+                assignees
+            });
+        } catch (error) {
+            core.setFailed(error.message);
+        }
     }
 };
 
